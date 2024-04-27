f2bae05aec6048ecb2fbb859f4bbec250d5cfbc6
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2024-04-27 21:57:18 +0800

    add visit model, update patient model

--- medicAI/models.py
+++ medicAI/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Patient(models.Model):
+
+    # Create constants to limit choices
+    BLOOD_TYPE = [
+        ('AB+', 'AB+'),
+        ('AB-', 'AB-'),
+        ('A+', 'A+'),
+        ('A-', 'A-'),
+        ('B+', 'B+'),
+        ('B-', 'B-'),
+        ('O+', 'O+'),
+        ('O-', 'O-'),
+    ]
+
+    SEX_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    ]
+
+    patient_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    dob = models.DateField()
+    address = models.CharField(max_length=64, blank=True)
+    phone_number = models.CharField(max_length=10, blank=True)
+    height = models.IntegerField(blank=True)
+    weight = models.IntegerField(blank=True)
+    sex = models.CharField(max_length=6, choices=SEX_CHOICES)
+    ohip = models.CharField(max_length=12)
+    blood_type = models.CharField(
+        max_length=3, 
+        choices=BLOOD_TYPE, 
+        default="", 
+        blank=True
+    )
+
+
+class Hospital_Visit(models.Model):
+
+    ACUITY_CHOICES = [
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+    ]
+        
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    symptom_start_date = models.DateField(default=timezone.now, blank=True)
+    acuity = models.CharField(max_length=1, choices=ACUITY_CHOICES, default='5')
+    temperature = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        null=True,
+        blank=True,
+        validators=[MinValueValidator(30), MaxValueValidator(45)]
+    )
+    heart_rate = models.IntegerField(null=True, blank=True)
+    perscription = models.CharField(max_length=200, blank=True)
+    diagnosis = models.CharField(max_length=200, blank=True)
+    seen_nurse = models.BooleanField(default=False)
+
+    
