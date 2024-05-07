36d4daaeacc7c6df804987bd107ccba75983586a
deschi0000 <deschi0000@outlook.com>
2024-05-08 02:38:00 +0800

    update index view

--- medicAI/views.py
+++ medicAI/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist, EmptyResultSet
+
+from medicAI.models import Hospital_Visit, Patient
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "medicAI/index.html")
+
+    # Use try/except to find patients in the db
+    # all_patients = Patient.objects.all()
+    # context = {
+    #     "patients": all_patients,
+    # }
+    # return render(request, "medicAI/index.html", context)
+
+    # Get all of the hospital visits and also the information
+    # of each of the patients as well
+    # all_hospital_visits = Hospital_Visit.objects.all()
+    all_hospital_visits = Hospital_Visit.objects.all().order_by('acuity')
+    print(all_hospital_visits)
+    context = {
+        "hospital_visits": all_hospital_visits
+    }
+    return render(request, "medicAI/index.html", context)
+
