1906c38a6c8f460c8ab04c63cc41e20592b86f7a
deschi0000 <deschi0000@outlook.com>
2024-05-09 08:13:51 +0800

    add patient info view

--- medicAI/views.py
+++ medicAI/views.py
@@ -25,3 +25,20 @@ def index(request):
     }
     return render(request, "medicAI/index.html", context)
 
+def patient_info(request, patient_id):
+    patient = Patient.objects.get(pk=patient_id)
+    # print(patient.first_name)
+
+    # Get the most recent visit
+    visit_list = Hospital_Visit.objects.filter(id=patient_id).order_by('-symptom_start_date')
+    most_recent_visit = visit_list.first()
+    # for i in visit_list:       # for debugging if checking for multiple visits.
+    #     print(i)
+
+    context= {
+        'patient': patient,
+        'visit_list' : visit_list,
+        'most_recent_visit': most_recent_visit
+    }
+
+    return render(request, "medicAI/patientinfo.html", context)
