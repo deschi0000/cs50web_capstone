5af0777d33ca0ca6e65821ee9898fac9c13c2d3d
deschi0000 <deschi0000@outlook.com>
2024-05-09 17:19:22 +0800

    add patientlist path

--- medicAI/urls.py
+++ medicAI/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("patientlist", views.patientlist, name="patientlist"),
     path("patientinfo/<str:patient_id>", views.patient_info, name="patientinfo")
 ]
